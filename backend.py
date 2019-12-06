c304755ae00ad99311d60cdfe286b2c0e4573a46
learningcode <learningcode9@gmail.com>
2019-12-06 07:46:54 -0500

    employee ide added

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ def create_table():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("CREATE TABLE IF NOT EXISTS entry(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime DATE,exittime DATE)")
-    cur.execute("CREATE TABLE IF NOT EXISTS entry1(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS entry1(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId TEXT NOT NULL,entrytime TEXT)")
     conn.commit()
     conn.close()
 
@@ -13,7 +13,7 @@ def create_table1():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("CREATE TABLE IF NOT EXISTS entry(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime DATE,exittime DATE)")
-    cur.execute("CREATE TABLE IF NOT EXISTS entry2(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,exittime TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS entry2(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId TEXT NOT NULL,exittime TEXT)")
     conn.commit()
     conn.close()
 
@@ -21,23 +21,24 @@ def create_table2():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
    # cur.execute("CREATE TABLE IF NOT EXISTS entry(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime DATE,exittime DATE)")
-    cur.execute("SELECT * INTO entry6 FROM(select e1.firstname,e1.lastname,e1.entrytime,e2.exittime from entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname)a")
+    #cur.execute("SELECT * INTO entry6 FROM(select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid)a")
+    cur.execute("SELECT * INTO entry3 FROM(SELECT e1.firstname, e1.lastname,e1.employeeid,e1.entrytime,e2.exittime FROM entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname and e1.lastname =e2.lastname and e1.employeeid=e2.employeeid)a")
     conn.commit()
     conn.close()
 
-def insert1(firstname,lastname,entrytime):
+def insert1(firstname,lastname,employeeid,entrytime):
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("INSERT INTO entry1 VALUES(%s,%s,%s)",(firstname,lastname,entrytime))
+    cur.execute("INSERT INTO entry1 VALUES(%s,%s,%s,%s)",(firstname,lastname,employeeid,entrytime))
     conn.commit()
     conn.close()
 
-def insert2(firstname,lastname,exittime):
+def insert2(firstname,lastname,employeeid,exittime):
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("INSERT INTO entry2 VALUES(%s,%s,%s)",(firstname,lastname,exittime))
+    cur.execute("INSERT INTO entry2 VALUES(%s,%s,%s,%s)",(firstname,lastname,employeeid,exittime))
     conn.commit()
     conn.close()
 
@@ -46,7 +47,7 @@ def view1():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("SELECT firstname,lastname,entrytime FROM entry1 order by entrytime DESC")
+    cur.execute("SELECT firstname,lastname,employeeid,entrytime FROM entry1 order by entrytime DESC")
     rows=cur.fetchall()
     conn.close()
     return rows
@@ -55,17 +56,17 @@ def view2():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("SELECT firstname,lastname,exittime FROM entry2 order by exittime DESC")
+    cur.execute("SELECT firstname,lastname,employeeid,exittime FROM entry2 order by exittime DESC")
     rows=cur.fetchall()
     conn.close()
     return rows
 
-def search(firstname="",lastname=""):
+def search(firstname="",lastname="",employeeid=""):
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
     #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
     #cur.execute("SELECT FROM entry3 WHERE firstname=? OR lastname=?",(firstname,lastname))
-    cur.execute("SELECT * FROM entry6 WHERE firstname=%s AND lastname=%s",(firstname,lastname))
+    cur.execute("SELECT * FROM entry3 WHERE firstname=%s OR  lastname=%s OR employeeid=%s",(firstname,lastname,employeeid))
     rows=cur.fetchall()
     conn.close()
     return rows
@@ -74,10 +75,11 @@ def search(firstname="",lastname=""):
 create_table()
 create_table1()
 #
-# create_table2()
+#create_table2()
 #insert('srasds','belem','2019-11-12','2019-11-10')
 #print(view())
 #print(search('sravani','bellam'))
+#print(view1())
 
 
 
--- daystart1.py
+++ daystart1.py
@@ -8,104 +8,149 @@ def select_date1():
     now=datetime.now()
     s1 = now.strftime("%Y-%m-%d , %I:%M %p")
     #print(s1)
-    if(first_name.get()=="" or last_name.get()==""):
+    if(first_name.get()=="" or last_name.get()=="" or employee_id.get()==""):
         pass
     else:
-        list1.insert(END,s1)
-        backend.insert1(first_name.get(),last_name.get(),s1)
-        list1.delete(0,END)
-        list1.insert(END,(first_name.get(),last_name.get(),s1))
+        listbox.insert(END,s1)
+        backend.insert1(first_name.get(),last_name.get(),employee_id.get(),s1)
+        listbox.delete(0,END)
+        listbox.insert(END,(first_name.get(),last_name.get(),employee_id.get(),s1))
 #def get_selected_row():
    
 def select_date2():
   
     now=datetime.now()
     s2 = now.strftime("%m/%d/%Y, %I:%M %p")
-    if(first_name.get()=="" or last_name.get()==""):
+    if(first_name.get()=="" or last_name.get()=="" or employee_id.get()==""):
         pass
     else:
-        list1.insert(END,s2)
-        backend.insert2(first_name.get(),last_name.get(),s2)
-        list1.delete(0,END)
-        list1.insert(END,(first_name.get(),last_name.get(),s2))
+        listbox.insert(END,s2)
+        backend.insert2(first_name.get(),last_name.get(),employee_id.get(),s2)
+        listbox.delete(0,END)
+        listbox.insert(END,(first_name.get(),last_name.get(),employee_id.get(),s2))
     
     
 
 def view_command():
-    list1.delete(0,END)
+    listbox.delete(0,END)
     for row in backend.view1():
-        list1.insert(END,row)
+        listbox.insert(END,row)
 
 def view_exit():
-    list1.delete(0,END)
+    listbox.delete(0,END)
     for row in backend.view2():
-        list1.insert(END,row)
+        listbox.insert(END,row)
 
 def search_command():
-    list1.delete(0,END)
-    list1.delete(0,END)
-    for row in backend.search(first_name.get(),last_name.get()):
-        list1.insert(END,row)
+    listbox.delete(0,END)
+    listbox.delete(0,END)
+    for row in backend.search(first_name.get(),last_name.get(),employee_id.get()):
+        listbox.insert(END,row)
 
 
-window = Tk()
+
 #print(type(window))
+window=Tk()
 window.wm_title("Library register")
-window.configure(background='#a9a9a9')
-window.resizable(width=False,height=False)
-
-width_of_window=480
-height_of_window=240
-
-screen_width=window.winfo_screenwidth()
-screen_height=window.winfo_screenheight()
-
-x_coordinates = (screen_width/2)-(width_of_window/2)
-y_coordinates = (screen_height/2)-(height_of_window/2)
-
-window.geometry("%dx%d+%d+%d"%(width_of_window,height_of_window,x_coordinates,y_coordinates))
-
-l1=Label(window,text='Firstname ',bg="#a9a9a9",font="TIMES 12 bold",pady=7,width=10)
+frame1=Frame(window)
+frame1.pack()
+    
+l1=Label(frame1,text="Enter Firstname",font="Times 12 bold",width=12)
 l1.grid(row=0,column=0)
-
-l2=Label(window,text='Lastname ',bg="#a9a9a9",font="TIMES 12 bold",pady=5,width=10)
-l2.grid(row=1,column=0)
+l2=Label(frame1,text="Enter Lastname",font="Times 12 bold",width=12)
+l2.grid(row=0,column=2)
+l3=Label(frame1,text="Enter EmployeeId",font="Times 12 bold",width=12)
+l3.grid(row=1,column=0)
 
 first_name=StringVar()
-e1=Entry(window,exportselection=0,width=25,textvariable=first_name)
+e1=Entry(frame1,width=25,textvariable=first_name)
 e1.grid(row=0,column=1)
-
 last_name=StringVar()
-e2=Entry(window,exportselection=0,width=25,textvariable=last_name)
-e2.grid(row=1,column=1)
+e2=Entry(frame1,width=25,textvariable=last_name)
+e2.grid(row=0,column=3)
+employee_id=StringVar()
+e3=Entry(frame1,width=25,textvariable=employee_id)
+e3.grid(row=1,column=1)
 
-list1=Listbox(window,height=9,width=48)
-list1.grid(row=2,column=0,padx=2,pady=1,rowspan=5,columnspan=4)
+frame2 = Frame(window)       
+frame2.pack()
 
-sb1=Scrollbar(window)
-sb1.grid(row=2,column=4,rowspan=6,padx=4)
+b1=Button(frame2,text="TimeIn",font="Times 12",width=9,command=select_date1)
+b1.grid(row=4,column=0)
+b2=Button(frame2,text="TimeOut",font="Times 12",width=9,command=select_date2)
+b2.grid(row=4,column=1)
+b3=Button(frame2,text="ViewEntries",font="Times 12",width=9,command=view_command)
+b3.grid(row=4,column=2)
+b3=Button(frame2,text="Viewexit",font="Times 12",width=9,command=view_exit)
+b3.grid(row=4,column=3)
+b4=Button(frame2,text="Search",font="Times 12",width=9,command=search_command)
+b4.grid(row=4,column=4)
+b5=Button(frame2,text="Close",font="Times 12",width=9,command=window.destroy)
+b5.grid(row=4,column=5)
 
-list1.configure(yscrollcommand=sb1.set)
-sb1.configure(command = list1.yview)
+
+frame3 = Frame(window)       
+frame3.pack()
+scroll = Scrollbar(frame3, orient=VERTICAL)
+listbox= Listbox(frame3, yscrollcommand=scroll.set,width=80,height=16)
+scroll.config(command=listbox.yview)
+scroll.pack(side=RIGHT, fill=Y)
+listbox.pack(side=LEFT, fill=BOTH, expand=1)
+#window.configure(background='#a9a9a9')
+window.resizable(width=False,height=False)
+
+#width_of_window=600
+#height_of_window=300
+
+#screen_width=window.winfo_screenwidth()
+#screen_height=window.winfo_screenheight()
+
+#x_coordinates = (screen_width/2)-(width_of_window/2)
+#y_coordinates = (screen_height/2)-(height_of_window/2)
+
+#window.geometry("%dx%d+%d+%d"%(width_of_window,height_of_window,x_coordinates,y_coordinates))
+
+#l1=Label(window,text='Firstname ',bg="#a9a9a9",font="TIMES 12 bold",pady=7,width=10)
+#l1.grid(row=0,column=0)
+
+#l2=Label(window,text='Lastname ',bg="#a9a9a9",font="TIMES 12 bold",pady=5,width=10)
+#l2.grid(row=1,column=0)
+
+#first_name=StringVar()
+#e1=Entry(window,exportselection=0,width=25,textvariable=first_name)
+#e1.grid(row=0,column=1)
+
+#last_name=StringVar()
+#e2=Entry(window,exportselection=0,width=25,textvariable=last_name)
+#e2.grid(row=1,column=1)
+
+#list1=Listbox(window,height=9,width=48)
+#list1.grid(row=2,column=0,padx=2,pady=1,rowspan=5,columnspan=4)
+
+#sb1=Scrollbar(window)
+#sb1.grid(row=2,column=4,rowspan=6,padx=4)
+
+#list1.configure(yscrollcommand=sb1.set)
+#sb1.configure(command = list1.yview)
 
 #list1.bind('<<ListboxSelect>>',get_selected_row)
 
-b1=Button(window,text="TimeIn", width=14,font="Times 12",command=select_date1)
-b1.grid(row=0,column=6)
+#b1=Button(window,text="TimeIn", width=14,font="Times 12",command=select_date1)
+#b1.grid(row=0,column=6)
 
-b2=Button(window,text="TimeOut", width=14,pady=5,font="Times 12",command=select_date2)
-b2.grid(row=1,column=6)
+#b2=Button(window,text="TimeOut", width=14,pady=5,font="Times 12",command=select_date2)
+#b2.grid(row=1,column=6)
 
-b3=Button(window,text="ViewEntries", width=14,pady=5,font="Times 12",command=view_command)
-b3.grid(row=2,column=6)
+#b3=Button(window,text="ViewEntries", width=14,pady=5,font="Times 12",command=view_command)
+#b3.grid(row=2,column=6)
 
-b4=Button(window,text="Viewexit", width=14,font="Times 12",command=view_exit)
-b4.grid(row=3,column=6)
+#b4=Button(window,text="Viewexit", width=14,font="Times 12",command=view_exit)
+#b4.grid(row=3,column=6)
 
-b5=Button(window,text="Search", width=14,pady=5,font="Times 12",command=search_command)
-b5.grid(row=4,column=6)
+#b5=Button(window,text="Search", width=14,pady=5,font="Times 12",command=search_command)
+#b5.grid(row=4,column=6)
 
-b6=Button(window,text="Close",width=14,font="Times 12",command=window.destroy)
-b6.grid(row=5,column=6)
+#b6=Button(window,text="Close",width=14,font="Times 12",command=window.destroy)
+#b6.grid(row=5,column=6)
 
 window.mainloop()
