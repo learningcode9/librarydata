8e8661fbad1876c29de51d14a838b2e4f573eaf4
learningcode <learningcode9@gmail.com>
2020-04-18 16:24:30 -0700

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,90 +1,84 @@
 import psycopg2
 
+
 #created tables
-def create_table():
+def entry_time():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("CREATE TABLE IF NOT EXISTS entry(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime DATE,exittime DATE)")
-    cur.execute("CREATE TABLE IF NOT EXISTS entry1(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId TEXT NOT NULL,entrytime TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS entryTime(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId text,entrytime text,day text)")
+    conn.commit()
+    conn.close()
+def exit_time():
+    conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS exitTime(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId text ,exittime text,day text)")
     conn.commit()
     conn.close()
 
-def create_table1():
+#Inserting data into the tables when you click timeIn and timeOut buttons
+def insert_entrytime(firstname,lastname,employeeid,entrytime,day):
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("CREATE TABLE IF NOT EXISTS entry(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,entrytime DATE,exittime DATE)")
-    cur.execute("CREATE TABLE IF NOT EXISTS entry2(Firstname TEXT NOT NULL,Lastname TEXT NOT NULL,EmployeeId TEXT NOT NULL,exittime TEXT)")
+    cur.execute("INSERT INTO entryTime VALUES(%s,%s,%s,%s,%s)",(firstname,lastname,employeeid,entrytime,day))
+    conn.commit()
+    conn.close()
+def insert_exittime(firstname,lastname,employeeid,exittime,day):
+    conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO exitTime VALUES(%s,%s,%s,%s,%s)",(firstname,lastname,employeeid,exittime,day))
     conn.commit()
     conn.close()
 
-#def create_table2():
- 
- #   conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
-  #  cur=conn.cursor()
-   # cur.execute("select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid")
-    #cur.execute("SELECT * INTO entry6 FROM(select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid)a")
-   # cur.execute("SELECT * INTO entry3 FROM(SELECT e1.firstname, e1.lastname,e1.employeeid,e1.entrytime,e2.exittime FROM entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname and e1.lastname =e2.lastname and e1.employeeid=e2.employeeid)a")
-    #cur.execute("SELECT * INTO entry3 FROM(SELECT e1.firstname, e1.lastname,e1.employeeid,e1.entrytime,e2.exittime FROM entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname and e1.lastname =e2.lastname and e1.employeeid=e2.employeeid)a WHERE EXISTS (SELECT * FROM entry3 WHERE firstname=%s OR  lastname=%s OR employeeid=%s",(firstname,lastname,employeeid)"))
-    #conn.commit()
-    #conn.close()
-
-
-def insert1(firstname,lastname,employeeid,entrytime):
+#Counting how many times the employee is entering and exiting
+def entry_count():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("INSERT INTO entry1 VALUES(%s,%s,%s,%s)",(firstname,lastname,employeeid,entrytime))
-    conn.commit()
+    cur.execute("select employeeid,count(entrytime) from  entryTime group by employeeid")  
+    rows=cur.fetchall()
     conn.close()
-
-def insert2(firstname,lastname,employeeid,exittime):
+    return rows
+def exit_count():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("INSERT INTO entry2 VALUES(%s,%s,%s,%s)",(firstname,lastname,employeeid,exittime))
-    conn.commit()
-    conn.close()
-
-
-def view1():
-    conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
-    cur=conn.cursor()
-    cur.execute("SELECT firstname,lastname,employeeid,entrytime FROM entry1 order by entrytime DESC")
+    cur.execute("select employeeid,count(exittime) from exitTime group by employeeid")
     rows=cur.fetchall()
     conn.close()
     return rows
 
-def view2():
+
+#displaying allemployees entrytimings and exittimings
+def all_employee_entry_details():
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    cur.execute("SELECT firstname,lastname,employeeid,exittime FROM entry2 order by exittime DESC")
+    cur.execute("SELECT firstname,lastname,employeeid,entrytime FROM entryTime order by entrytime DESC")
     rows=cur.fetchall()
     conn.close()
     return rows
+def all_employee_exit_details():
+    conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
+    cur=conn.cursor()
+    cur.execute("SELECT firstname,lastname,employeeid,exittime FROM  exitTime order by exittime DESC ")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+#Searching the employees based on the firstname,lastname or employeeid
 
 def search(firstname="",lastname="",employeeid=""):
     conn=psycopg2.connect("dbname='library_data' user='postgres' password='postgres1984' host='localhost' port='5432'")
     cur=conn.cursor()
-    #cur.execute("INSERT INTO entry1 VALUES(%s,%s)",(firstname,lastname))
-    #cur.execute("SELECT FROM entry3 WHERE firstname=? OR lastname=?",(firstname,lastname))
-    cur.execute("SELECT * FROM (select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid) AS new_table WHERE firstname=%s OR  lastname=%s OR employeeid=%s",(firstname,lastname,employeeid))
-    #cur.execute("SELECT e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime FROM entry1 e1 INNER JOIN entry2 e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid WHERE firstname =e2.firstname OR e1.lastname =e2.lastname OR e1.employeeid=e2.employeeid")
+    cur.execute("SELECT * FROM (select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entryTime e1 INNER JOIN  exitTime e2 ON e1.firstname=e2.firstname AND e1.lastname=e2.lastname AND e1.employeeid=e2.employeeid) AS new_table WHERE firstname=%s OR  lastname=%s OR employeeid=%s",(firstname,lastname,employeeid))
+    #cur.exexute("select e1.firstname,e1.lastname,e1.employeeid,e1.entrytime,e2.exittime from entryTime e1 INNER JOIN exitTime e2 on e1.firstname=e2.firstname and e1.lastname=e2.lastname,e1.employeeid=e2.employeeid")
     rows=cur.fetchall()
     conn.close()
     return rows
 
- 
-create_table()
-create_table1()
+entry_time()
+exit_time()
 
-#search()
-#
-#create_table2()
-#insert('srasds','belem','2019-11-12','2019-11-10')
-#print(view())
 
-#print(view1())
+
+
 
 
 
